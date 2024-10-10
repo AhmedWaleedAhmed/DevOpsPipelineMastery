a030a1207f68a3b65854a74c4405c4345128006e
AhmedWaleedAhmed <ahmdwlydahmd09@gmail.com>
2024-10-10 22:12:38 +0300

    Refine how Terraform works to build the inventory on his own.

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,304 +5,272 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_vpc" "waleed_vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.10.10.0/24"
   
   enable_dns_support   = true
   enable_dns_hostnames = true
   
   tags = {
-    Name = "waleed-vpc"
+    Name = "vpc-${var.tag_suffix}"
   }
 }
 
-resource "aws_subnet" "waleed_subnet" {
-  vpc_id                  = aws_vpc.waleed_vpc.id
+resource "aws_subnet" "subnet" {
+  vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.10.10.0/25"  # Subnet CIDR must be a subset of VPC CIDR
   availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "waleed-subnet-public1"
+    Name = "public-subnet-${var.tag_suffix}"
   }
 }
 
-resource "aws_internet_gateway" "waleed_igw" {
-  vpc_id = aws_vpc.waleed_vpc.id
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
 
   tags = {
-    Name = "waleed-igw"
+    Name = "igw-${var.tag_suffix}"
   }
 }
 
-resource "aws_route_table" "waleed_route_table" {
-  vpc_id = aws_vpc.waleed_vpc.id
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block                = "0.0.0.0/0"
-    gateway_id                = aws_internet_gateway.waleed_igw.id
+    gateway_id                = aws_internet_gateway.igw.id
   }
 
   tags = {
-    Name = "waleed-route-table"
+    Name = "route-table-${var.tag_suffix}"
   }
 }
 
-resource "aws_route_table_association" "waleed_route_table_assoc" {
-  subnet_id      = aws_subnet.waleed_subnet.id
-  route_table_id = aws_route_table.waleed_route_table.id
+resource "aws_route_table_association" "route_table_assoc" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route_table.id
 }
 
-resource "aws_security_group" "waleed_security_group" {
-  vpc_id = aws_vpc.waleed_vpc.id
-
-  name        = "waleed-security-group"
-  description = "Default security group for waleed-vpc"
-
-  ingress {
-    from_port   = 3000
-    to_port     = 10000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 25
-    to_port     = 25
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 465
-    to_port     = 465
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 30000
-    to_port     = 32767
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  # All traffic
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group" "security_group" {
+  vpc_id      = aws_vpc.vpc.id
+  name        = "security-group-${var.tag_suffix}"
+  description = "Default security group for vpc-${var.tag_suffix}"
 
   tags = {
-    Name = "waleed-security-group"
+    Name = "security-group-${var.tag_suffix}"
   }
 }
+
+# Ingress Rules
+resource "aws_security_group_rule" "allow_tcp_3000_to_10000" {
+  type              = "ingress"
+  from_port        = 3000
+  to_port          = 10000
+  protocol         = "tcp"
+  security_group_id = aws_security_group.security_group.id
+  cidr_blocks      = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_tcp_6443" {
+  type              = "ingress"
+  from_port        = 6443
+  to_port          = 6443
+  protocol         = "tcp"
+  security_group_id = aws_security_group.security_group.id
+  cidr_blocks      = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_tcp_22" {
+  type              = "ingress"
+  from_port        = 22
+  to_port          = 22
+  protocol         = "tcp"
+  security_group_id = aws_security_group.security_group.id
+  cidr_blocks      = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_tcp_443" {
+  type              = "ingress"
+  from_port        = 443
+  to_port          = 443
+  protocol         = "tcp"
+  security_group_id = aws_security_group.security_group.id
+  cidr_blocks      = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_tcp_25" {
+  type              = "ingress"
+  from_port        = 25
+  to_port          = 25
+  protocol         = "tcp"
+  security_group_id = aws_security_group.security_group.id
+  cidr_blocks      = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_tcp_80" {
+  type              = "ingress"
+  from_port        = 80
+  to_port          = 80
+  protocol         = "tcp"
+  security_group_id = aws_security_group.security_group.id
+  cidr_blocks      = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_tcp_465" {
+  type              = "ingress"
+  from_port        = 465
+  to_port          = 465
+  protocol         = "tcp"
+  security_group_id = aws_security_group.security_group.id
+  cidr_blocks      = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_tcp_30000_to_32767" {
+  type              = "ingress"
+  from_port        = 30000
+  to_port          = 32767
+  protocol         = "tcp"
+  security_group_id = aws_security_group.security_group.id
+  cidr_blocks      = ["0.0.0.0/0"]
+}
+
+# Egress Rule
+resource "aws_security_group_rule" "allow_all_egress" {
+  type              = "egress"
+  from_port        = 0
+  to_port          = 0
+  protocol         = "-1"  # All traffic
+  security_group_id = aws_security_group.security_group.id
+  cidr_blocks      = ["0.0.0.0/0"]
+}
+
 
 resource "aws_instance" "jenkins" {
-  ami           = "ami-0745b7d4092315796"
-  instance_type = "t2.large"
-  key_name      = "ahmedwaleed"
-  subnet_id     = aws_subnet.waleed_subnet.id
-  vpc_security_group_ids = [aws_security_group.waleed_security_group.id]
-	root_block_device {
-		delete_on_termination = true
-		encrypted             = false
-		kms_key_id            = null
-		tags                  = {}
-		tags_all              = {}
-		throughput            = 0
-		volume_size           = 30
-		volume_type           = "gp2"
-	}
-  tags = {
-    Name = "waleed-jenkins"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.large_instance_type
+  key_name      = var.key_name
+  subnet_id     = aws_subnet.subnet.id
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+  root_block_device {
+    volume_size = var.volume_size
+    volume_type = var.volume_type
   }
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorized_keys
-    deluser --remove-home ubuntu
-    EOF
+  tags = {
+    Name = "jenkins-${var.tag_suffix}"
+  }
+  user_data = file("${path.module}/${var.user_data_file_path}")
 }
 
 resource "aws_instance" "nexus" {
-  ami           = "ami-0745b7d4092315796"
-  instance_type = "t2.medium"
-  key_name      = "ahmedwaleed"
-  subnet_id     = aws_subnet.waleed_subnet.id
-  vpc_security_group_ids = [aws_security_group.waleed_security_group.id]
-	root_block_device {
-		delete_on_termination = true
-		encrypted             = false
-		kms_key_id            = null
-		tags                  = {}
-		tags_all              = {}
-		throughput            = 0
-		volume_size           = 25
-		volume_type           = "gp2"
-	}
-  tags = {
-    Name = "waleed-nexus"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.medium_instance_type
+  key_name      = var.key_name
+  subnet_id     = aws_subnet.subnet.id
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+  root_block_device {
+    volume_size = var.volume_size
+    volume_type = var.volume_type
   }
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorized_keys
-    deluser --remove-home ubuntu
-    EOF
+  tags = {
+    Name = "nexus-${var.tag_suffix}"
+  }
+  user_data = file("${path.module}/${var.user_data_file_path}")
 }
 
 resource "aws_instance" "sonarQube" {
-  ami           = "ami-0745b7d4092315796"
-  instance_type = "t2.medium"
-  key_name      = "ahmedwaleed"
-  subnet_id     = aws_subnet.waleed_subnet.id
-  vpc_security_group_ids = [aws_security_group.waleed_security_group.id]
-	root_block_device {
-		delete_on_termination = true
-		encrypted             = false
-		kms_key_id            = null
-		tags                  = {}
-		tags_all              = {}
-		throughput            = 0
-		volume_size           = 25
-		volume_type           = "gp2"
-	}
-  tags = {
-    Name = "waleed-sonarQube"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.medium_instance_type
+  key_name      = var.key_name
+  subnet_id     = aws_subnet.subnet.id
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+  root_block_device {
+    volume_size = var.volume_size
+    volume_type = var.volume_type
   }
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorized_keys
-    deluser --remove-home ubuntu
-    EOF
+  tags = {
+    Name = "sonarQube-${var.tag_suffix}"
+  }
+  user_data = file("${path.module}/${var.user_data_file_path}")
 }
 
 resource "aws_instance" "k8s-master" {
-  ami           = "ami-0745b7d4092315796"
-  instance_type = "t2.medium"
-  key_name      = "ahmedwaleed"
-  subnet_id     = aws_subnet.waleed_subnet.id
-  vpc_security_group_ids = [aws_security_group.waleed_security_group.id]
-	root_block_device {
-		delete_on_termination = true
-		encrypted             = false
-		kms_key_id            = null
-		tags                  = {}
-		tags_all              = {}
-		throughput            = 0
-		volume_size           = 25
-		volume_type           = "gp2"
-	}
-  tags = {
-    Name = "waleed-k8s-master"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.medium_instance_type
+  key_name      = var.key_name
+  subnet_id     = aws_subnet.subnet.id
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+  root_block_device {
+    volume_size = var.volume_size
+    volume_type = var.volume_type
   }
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorized_keys
-    deluser --remove-home ubuntu
-    EOF
+  tags = {
+    Name = "k8s-master-${var.tag_suffix}"
+  }
+  user_data = file("${path.module}/${var.user_data_file_path}")
 }
 
 resource "aws_instance" "k8s-worker-1" {
-  ami           = "ami-0745b7d4092315796"
-  instance_type = "t2.medium"
-  key_name      = "ahmedwaleed"
-  subnet_id     = aws_subnet.waleed_subnet.id
-  vpc_security_group_ids = [aws_security_group.waleed_security_group.id]
-	root_block_device {
-		delete_on_termination = true
-		encrypted             = false
-		kms_key_id            = null
-		tags                  = {}
-		tags_all              = {}
-		throughput            = 0
-		volume_size           = 25
-		volume_type           = "gp2"
-	}
-  tags = {
-    Name = "waleed-k8s-worker-1"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.medium_instance_type
+  key_name      = var.key_name
+  subnet_id     = aws_subnet.subnet.id
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+  root_block_device {
+    volume_size = var.volume_size
+    volume_type = var.volume_type
   }
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorized_keys
-    deluser --remove-home ubuntu
-    EOF
+  tags = {
+    Name = "k8s-worker-1-${var.tag_suffix}"
+  }
+  user_data = file("${path.module}/${var.user_data_file_path}")
 }
 
 resource "aws_instance" "k8s-worker-2" {
-  ami           = "ami-0745b7d4092315796"
-  instance_type = "t2.medium"
-  key_name      = "ahmedwaleed"
-  subnet_id     = aws_subnet.waleed_subnet.id
-  vpc_security_group_ids = [aws_security_group.waleed_security_group.id]
-	root_block_device {
-		delete_on_termination = true
-		encrypted             = false
-		kms_key_id            = null
-		tags                  = {}
-		tags_all              = {}
-		throughput            = 0
-		volume_size           = 25
-		volume_type           = "gp2"
-	}
-  tags = {
-    Name = "waleed-k8s-worker-2"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.medium_instance_type
+  key_name      = var.key_name
+  subnet_id     = aws_subnet.subnet.id
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+  root_block_device {
+    volume_size = var.volume_size
+    volume_type = var.volume_type
   }
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorized_keys
-    deluser --remove-home ubuntu
-    EOF
+  tags = {
+    Name = "k8s-worker-2-${var.tag_suffix}"
+  }
+  user_data = file("${path.module}/${var.user_data_file_path}")
 }
 
-# output "vpc_id" {
-#   value = aws_vpc.waleed_vpc.id
-# }
+resource "aws_instance" "monitoring" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.medium_instance_type
+  key_name      = var.key_name
+  subnet_id     = aws_subnet.subnet.id
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+	root_block_device {
+    volume_size = var.volume_size
+    volume_type = var.volume_type
+  }
+  tags = {
+    Name = "monitoring-${var.tag_suffix}"
+  }
+  user_data = file("${path.module}/${var.user_data_file_path}")
+}
 
-# output "subnet_id" {
-#   value = aws_subnet.waleed_subnet.id
-# }
+resource "local_file" "ansible_inventory" {
+  content = templatefile("${path.module}/templates/inventory.tpl", {
+    master_ip   = aws_instance.k8s-master.public_ip
+    worker1_ip  = aws_instance.k8s-worker-1.public_ip
+    worker2_ip  = aws_instance.k8s-worker-2.public_ip
+    sonarqube_ip = aws_instance.sonarQube.public_ip
+    nexus_ip    = aws_instance.nexus.public_ip
+    jenkins_ip  = aws_instance.jenkins.public_ip
+    monitoring_ip = aws_instance.monitoring.public_ip
+  })
 
-# output "internet_gateway_id" {
-#   value = aws_internet_gateway.waleed_igw.id
-# }
+  filename = "${path.module}/../ansible/inventory.yml"
+}
 
-# output "route_table_id" {
-#   value = aws_route_table.waleed_route_table.id
-# }
 
-# output "security_group_id" {
-#   value = aws_security_group.waleed_security_group.id
-# }
-
-# output "instance_id" {
-#   value = aws_instance.waleed_ec2.id
-# }
